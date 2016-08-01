8769a43b202c662f07cef9a5d199d509bb77cce9
karl-denby <karl.denby@gmail.com>
2016-08-01 14:11:49 +0100

    add class to hold story title/author/url

---
+++ app/src/main/java/com/example/android/news/Story.java
@@ -0,0 +1,19 @@
+package com.example.android.news;
+
+public class Story {
+    private String mHeadline;
+    private String mAuthor;
+    private String mURL;
+
+    public Story(String headline, String author, String url) {
+        mHeadline= headline;
+        mAuthor = author;
+        mURL = url;
+    }
+
+    public String getTitle() {return mHeadline;}
+
+    public String getAuthor() {return mAuthor;}
+
+    public String getURL() {return mURL;}
+}
